2b7ad2553027ae4bc7851a2fe17b5749fd8fe6bc
Ahmad Raza <ahmadsheikh0568@gmail.com>
2023-05-17 16:17:20 +0500

    first inti

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import MovieList from './pages/MovieList'
+
+function App() {
+
+  return (
+    <>
+      <MovieList />
+    </>
+  )
+}
+
+export default App
